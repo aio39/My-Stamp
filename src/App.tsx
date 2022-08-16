0e1134a390418333f287aaca816572a70dcb7425
Aio <AioDev.JS@gmail.com>
2022-08-16 19:44:38 +0900

    add: image input modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import 'antd/dist/antd.css';
 import './App.css';
 import SearchInput from './components/SearchInput';
+import NewIconInputModalButton from './components/templates/NewIconInputModalButton';
 
 function App() {
   return (
     <>
       <SearchInput />
+      <NewIconInputModalButton />
     </>
   );
 }
--- src/components/templates/NewIconInputModalButton.tsx
+++ src/components/templates/NewIconInputModalButton.tsx
@@ -1,11 +1,49 @@
-import { useState } from 'react';
+import { Button, Modal } from 'antd';
+import React, { useState } from 'react';
+import ImageUploadBox from '../atoms/ImageUploadBox';
+import NewTagsInput from '../organisms/NewTagsInput';
 
-const NewIconInputModalButton = () => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+const NewIconInputModalButton: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState('Content of the modal');
 
-  return <></>;
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal with async logic
+      </Button>
+      <Modal
+        title="Title"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <p>{modalText}</p>
+        <ImageUploadBox />
+        <NewTagsInput />
+      </Modal>
+    </>
+  );
 };
 
 export default NewIconInputModalButton;
